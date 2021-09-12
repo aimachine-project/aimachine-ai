c1c63c3c15714b10fc6202d169d54e83f3de081c
Sebastian Syska <s.syska@samsung.com>
2021-09-12 15:13:25 +0200

    add simple server messages handling

--- aimachine/server.py
+++ aimachine/server.py
@@ -1,6 +1,5 @@
 import json
 import multiprocessing
-import time
 from typing import Dict, List
 
 import flask
@@ -15,26 +14,43 @@ TICTACTOE_URL = 'ws://localhost:8080/games/tictactoe'
 
 @APP.route("/")
 def health_check():
-    return "<p>Hello, World!</p>"
+    return "<h1>Greetings from Aimachine AI!</h1>", 200
 
 
-def on_message(ws, message):
-    print(message)
+def on_message(ws, event):
+    data = json.loads(event)
+    event_type = data['eventType']
+    event_message = data['eventMessage']
+    print('event message: ' + event_message)
+    if event_type == 'game_id':
+        pass
+    elif event_type == 'client_id':
+        pass
+    elif event_type == 'field_to_be_marked':
+        field_data = json.loads(event_message)
+        row_index = field_data['rowIndex']
+        col_index = field_data['colIndex']
+        print('rowIndex, colIndex: {}-{}'.format(row_index, col_index))
+        pass
+    elif event_type == 'movement_allowed':
+        pass
+    elif event_type == 'server_message':
+        pass
+    else:
+        pass
 
 
 def on_error(ws, error):
-    print(error)
+    print('error: '.format(error))
 
 
 def on_close(ws, close_status_code, close_msg):
-    print("### closed ###")
+    print("client socket closed")
     PROCESSES[ws].terminate()
 
 
 def on_open(ws):
-    while True:
-        time.sleep(1)
-        ws.send(json.dumps({"eventType": "hello"}))
+    print("client socket open")
 
 
 def create_ws(ws):
@@ -43,7 +59,6 @@ def create_ws(ws):
 
 @APP.route("/tictactoe")
 def connect_computer_player():
-    print("yo yo")
     websocket.enableTrace(True)
     ws = websocket.WebSocketApp(TICTACTOE_URL,
                                 on_open=on_open,
@@ -57,7 +72,7 @@ def connect_computer_player():
     process.start()
     # process.join()
     PROCESSES[ws] = process
-    return "OK"
+    return "AI client created", 201
 
 
 if __name__ == '__main__':
