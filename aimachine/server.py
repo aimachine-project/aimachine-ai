0218756c2835ff2770c99afab52b631c1b1a878c
Sebastian Syska <s.syska@samsung.com>
2021-09-12 19:36:24 +0200

    AI plays randomly also on 14x14

--- aimachine/server.py
+++ aimachine/server.py
@@ -24,64 +24,66 @@ def health_check():
     return "<h1>Greetings from Aimachine AI!</h1>", 200
 
 
-def on_open(ws):
+def on_open(socket):
     print("client socket open")
-    BOARDS[ws] = BLANK_VALUE * np.zeros((3, 3), int)
+    BOARDS[socket] = BLANK_VALUE * np.zeros((3, 3), int)
 
 
-def on_open_extended(ws):
+def on_open_extended(socket):
     print("client socket open")
-    BOARDS[ws] = BLANK_VALUE * np.zeros((14, 14), int)
+    BOARDS[socket] = BLANK_VALUE * np.zeros((14, 14), int)
 
 
-def on_message(ws: websocket.WebSocket, event: str):
+def on_message(socket: websocket.WebSocket, event: str):
     data = json.loads(event)
     event_type = data['eventType']
     event_message = data['eventMessage']
     print('event message: ' + event_message)
     if event_type == 'game_id':
-        GAME_IDS[ws] = event_message
+        GAME_IDS[socket] = event_message
     elif event_type == 'client_id':
-        CLIENT_IDS[ws] = event_message
+        CLIENT_IDS[socket] = event_message
     elif event_type == 'field_to_be_marked':
         field_data = json.loads(event_message)
         row_index = field_data['rowIndex']
         col_index = field_data['colIndex']
         field_token = field_data['fieldToken']
         print('rowIndex, colIndex: {}-{}'.format(row_index, col_index))
-        BOARDS[ws][row_index, col_index] = field_token
+        BOARDS[socket][row_index, col_index] = field_token
 
     elif event_type == 'movement_allowed':
-        if event_message == CLIENT_IDS[ws]:
-            row_indices, col_indices = np.where(BOARDS[ws] == BLANK_VALUE)
+        if event_message == CLIENT_IDS[socket]:
+            row_indices, col_indices = np.where(BOARDS[socket] == BLANK_VALUE)
             free_pairs = list(zip(row_indices, col_indices))
             field_to_click = random.choice(free_pairs)
             data_to_send = json.dumps({
                 'eventType': 'field_clicked',
                 'eventMessage': {
-                    'gameId': GAME_IDS[ws],
+                    'gameId': GAME_IDS[socket],
                     'rowIndex': str(field_to_click[0]),
                     'colIndex': str(field_to_click[1])
                 }
             })
-            ws.send(data_to_send)
+            socket.send(data_to_send)
     elif event_type == 'server_message':
         print(event_message)
     else:
         print('unhandled message occurred')
 
 
-def on_error(ws, error):
-    print('error: '.format(error))
+def on_error():
+    print('error occurred')
 
 
-def on_close(ws, close_status_code, close_msg):
+def on_close(socket, close_status_code, close_msg):
     print("client socket closed")
-    PROCESSES[ws].terminate()
+    print("close code: {}".format(close_status_code))
+    print("close message: {}".format(close_msg))
+    PROCESSES[socket].terminate()
 
 
-def create_ws(ws):
-    ws.run_forever()
+def run_websocket_app(socket):
+    socket.run_forever()
 
 
 @APP.route("/tictactoe")
@@ -93,9 +95,9 @@ def connect_ai_tictactoe():
                                     on_error=on_error,
                                     on_close=on_close)
     WEBSOCKET_CLIENTS.append(client)
-    process_name = 'ws_{}'.format(WEBSOCKET_CLIENTS.index(client))
+    process_name = 'ws_process_{}'.format(WEBSOCKET_CLIENTS.index(client))
     print('process name: {}'.format(process_name))
-    process = multiprocessing.Process(name=process_name, target=create_ws, args=(client,), daemon=True)
+    process = multiprocessing.Process(name=process_name, target=run_websocket_app, args=(client,), daemon=True)
     process.start()
     PROCESSES[client] = process
     return "AI client created", 201
@@ -112,7 +114,7 @@ def connect_ai_tictactoe_extended():
     WEBSOCKET_CLIENTS.append(client)
     process_name = 'ws_{}'.format(WEBSOCKET_CLIENTS.index(client))
     print('process name: {}'.format(process_name))
-    process = multiprocessing.Process(name=process_name, target=create_ws, args=(client,), daemon=True)
+    process = multiprocessing.Process(name=process_name, target=run_websocket_app, args=(client,), daemon=True)
     process.start()
     PROCESSES[client] = process
     return "AI client created", 201
